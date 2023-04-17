191f6218fa6a8a9b35a3248130dc94b5503e6fa9
Anuj Singh <92370865+AnujSsStw@users.noreply.github.com>
2023-04-18 01:04:32 +0530

    added the Head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import NextDocument, { Html, Main, NextScript } from "next/document";
+import Head from 'next/head'
 import { ColorModeScript } from "@chakra-ui/react";
 import theme from "../theme";
 
